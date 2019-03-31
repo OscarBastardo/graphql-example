7f641e9d30086c00073aeb8c97863daafae87ea1
Oscar Bastardo <osalbr.14@gmail.com>
2019-03-31 20:32:49 +0100

    get book list from author

--- service/src/schema/Author.ts
+++ service/src/schema/Author.ts
@@ -1,11 +1,21 @@
-import { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLList } from 'graphql';
+import { filter } from 'lodash';
+import BookType from './Book';
+
+const books = require('../data/books.json');
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return filter(books, { authorId: parent.id })
+      }
+    }
   })
 });
 
