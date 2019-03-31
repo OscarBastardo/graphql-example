1994479aec89d0c8a41b4706a30d7605fe4a5c6f
Oscar Bastardo <osalbr.14@gmail.com>
2019-03-31 23:05:32 +0100

    add form to create new book with dynamic author list

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
-import BookList from './components/BookList'
+import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
-})
+});
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <div className="App">
             <h1>Oscar's Reading List</h1>
             <BookList />
+            <AddBook />
           </div>
       </ApolloProvider>
     );
