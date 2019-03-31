ee66ff7b589f45ba59a4449864ab4b7502cfe7f1
Oscar Bastardo <osalbr.14@gmail.com>
2019-03-31 22:47:28 +0100

    delete root package.json file and add cors to the koa server

--- service/src/server/createServer.ts
+++ service/src/server/createServer.ts
@@ -1,11 +1,14 @@
 import * as Koa from 'koa';
 import * as mount from 'koa-mount';
 import * as graphqlHTTP from 'koa-graphql';
+import * as cors from '@koa/cors';
 
 import schema from '../schema';
 
 function createServer() {
   const server = new Koa();
+
+  server.use(cors());
 
   server.use(mount('/graphql', graphqlHTTP({
     schema,
