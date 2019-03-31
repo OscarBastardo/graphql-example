badea8e8d964b6854b6dd0dbab30ebf4eb2d2ab9
Oscar Bastardo <osalbr.14@gmail.com>
2019-03-31 21:01:39 +0100

    add mongodb connection with mongoose

--- service/src/bin/index.ts
+++ service/src/bin/index.ts
@@ -1,8 +1,16 @@
 import * as dotenv from 'dotenv';
+import * as mongoose from 'mongoose';
 
 import createServer from 'src/server/createServer';
 
 dotenv.config();
+
+const dbUri = 'mongodb+srv://graphql:test123@cluster0-uqt1i.mongodb.net/test?retryWrites=true';
+mongoose.connect(dbUri, { useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  // tslint:disable-next-line: no-console
+  console.log('connected to database');
+})
 
 const PORT = process.env.PORT || 4000;
 const server = createServer();
