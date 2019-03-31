197a3641424d039672cc025a06e7f8280b3e1f16
Oscar Bastardo <osalbr.14@gmail.com>
2019-03-31 20:20:44 +0100

    get book with author details

--- service/src/schema/index.ts
+++ service/src/schema/index.ts
@@ -1,8 +1,10 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLID } from 'graphql';
 import { find } from 'lodash';
 import BookType from './Book';
+import AuthorType from './Author';
 
 const books = require('../data/books');
+const authors = require('../data/authors');
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -17,6 +19,18 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // code to get data from db / other source
         return find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: {
+          type: GraphQLID
+        }
+      },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return find(authors, { id: args.id })
       }
     }
   },
